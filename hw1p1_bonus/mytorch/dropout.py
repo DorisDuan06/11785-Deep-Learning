9d28d59cd2ac0ffccf6ac8f7d1dffdd4851ae1d0
Doris Duan <yanjiad@andrew.cmu.edu>
2020-05-08 02:51:28 -0400

    Add files via upload

---
+++ hw1p1_bonus/mytorch/dropout.py
@@ -0,0 +1,26 @@
+# Do not import any additional 3rd party external libraries as they will not
+# be available to AutoLab and are not needed (or allowed)
+
+import numpy as np
+
+
+class Dropout(object):
+    def __init__(self, p=0.5):
+        # Dropout probability
+        self.p = p
+
+    def __call__(self, x):
+        return self.forward(x)
+
+    def forward(self, x, train=True):
+        # 1) Get and apply a mask generated from np.random.binomial
+        # 2) Scale your output accordingly
+        # 3) During test time, you should not apply any mask or scaling.
+        self.mask = np.random.binomial(1, self.p, x.shape)
+        if train is True:
+            x *= self.mask / self.p
+        return x
+
+    def backward(self, delta):
+        # 1) This method is only called during trianing.
+        return delta * self.mask
